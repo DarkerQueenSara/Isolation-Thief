eba51d97d4d62f7f96ca313f7ba9de6e5dcc20bd
Carlos Marques <catralitos11@gmail.com>
2020-11-14 17:26:58 +0000

    Testei tudo, só não vi merges, mas deve dar certo

--- Assets/Scripts/teste2.cs
+++ Assets/Scripts/teste2.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class teste : MonoBehaviour
+public class teste2 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        //comentario de teste
+        
     }
 
     // Update is called once per frame
