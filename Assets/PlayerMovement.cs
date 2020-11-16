c231c73fa08c11f256ee9577ca9c1ee5df987cbb
DanielCorreia21 <LostTimesEraProject@users.noreply.github.com>
2020-11-16 16:36:57 +0000

    Basic controls for player with mini map to test

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public float speed = 12f;
+    public float gravity = -9.81f * 2;
+    public float jumpHeight = 2;
+
+    public Transform groundCheck;
+    public LayerMask groundMask;
+    public float groundDistance = 0.4f;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //verify if is on ground------
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        //-----------------------------
+
+
+        //player movement in x and z axis----
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+        //-----------------------------
+
+        //player jump------------------
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+        //-----------------------------
+
+
+        //gravity stuff-------
+        velocity.y += gravity * Time.deltaTime;
+
+        //fall velocity = g * t^2
+        controller.Move(velocity * Time.deltaTime);
+        //--------------------
+    }
+}
