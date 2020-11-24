cee2a3bf4bc926605856463288ffea58df7a5ea6
Bernardo Pinto <bernardo.ra.pinto@gmail.com>
2020-11-24 17:30:16 +0000

    Merge fix, WIP Inventory

--- Assets/Scripts/Player/Inventory/UI_Inventory.cs
+++ Assets/Scripts/Player/Inventory/UI_Inventory.cs
@@ -13,6 +13,9 @@ public class UI_Inventory : MonoBehaviour
     {
         itemSlotContainer = transform.Find("itemSlotContainer");
         itemSlotTemplate = itemSlotContainer.Find("itemSlotTemplate");
+
+        //Debug.Log("itemSlotContainer is null? " + itemSlotContainer == null);
+        //Debug.Log("itemSlotTemplate is null? " + itemSlotTemplate == null);
     }
 
     public void SetInventory(Inventory inventory)
@@ -29,7 +32,8 @@ public class UI_Inventory : MonoBehaviour
         float itemSlotCellSize = 30.0f;
         foreach(Item item in inventory.GetItemList())
         {
-            RectTransform itemSlotRectTransform = Instantiate(itemSlotTemplate, itemSlotContainer).GetComponent<RectTransform>();
+            Transform temp = Instantiate(itemSlotTemplate, itemSlotContainer);
+            RectTransform itemSlotRectTransform = temp.GetComponent<RectTransform>();
             itemSlotRectTransform.gameObject.SetActive(true);
             itemSlotRectTransform.anchoredPosition = new Vector2(x * itemSlotCellSize, y * itemSlotCellSize);
             Image image = itemSlotRectTransform.Find("image").GetComponent<Image>();
