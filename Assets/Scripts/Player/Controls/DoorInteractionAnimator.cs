1457547937f87d0fcb1592d587fad9af02e86d51
Bernardo Pinto <bernardo.ra.pinto@gmail.com>
2020-12-07 20:25:59 +0000

    CORRECTLY MERGE DOORINTERACTIONANIMATOR BY HAND
    Add new scene that is basicly the scene from gadget_lockpick

--- Assets/Scripts/Player/Controls/DoorInteractionAnimator.cs
+++ Assets/Scripts/Player/Controls/DoorInteractionAnimator.cs
@@ -3,20 +3,106 @@ using System.Collections;
 
 namespace Assets.Scripts.Player.Controls
 {
-	public class DoorInteractionAnimator : Interactable
-	{
-		public Animator animator;
+    public class DoorInteractionAnimator : Interactable
+    {
+        public Animator animator;
 
-		public override void interact()
-		{
-			bool isOpen = animator.GetBool("isOpenDoor");
-			animator.SetBool("isOpenDoor", !isOpen);
-		}
+        //Public 
+        public bool isLocked;
 
-		public override string getInteractingText()
-		{
-			bool isOpen = animator.GetBool("isOpenDoor");
-			return isOpen ? "Close door" : "Open door";
-		}
-	}
+        private bool lockpicking;
+
+        private void Awake()
+        {
+            lockpicking = false;
+        }
+
+
+        public override void interact()
+        {
+            if (isLocked)
+            {
+                if (lockpickable)
+                {
+                    if (player.CanLockpick())
+                    {
+                        this.StartLockpick();
+                    }
+                }
+
+
+            }
+            else
+            {
+                this.OpenOrClose();
+            }
+        }
+
+        //Look
+        public override string getInteractingText()
+        {
+
+            bool isOpen = animator.GetBool("isOpenDoor");
+
+            if (isLocked && isOpen)
+            {
+                Debug.LogError("Door is Locked and Open!");
+                return "Close door";
+            }
+
+            if (isLocked && !isOpen)
+            {
+                if (lockpickable)
+                {
+                    if (player.CanLockpick())
+                    {
+                        return "Lockpick";
+                    }
+                }
+
+                return "Locked";
+
+            }
+            else
+            {
+
+                return isOpen ? "Close door" : "Open door";
+            }
+        }
+
+        private void StartLockpick()
+        {
+            lockpicking = true;
+        }
+
+        private void Update()
+        {
+            if (lockpicking)
+            {
+                if (Input.GetButtonUp("Interact"))
+                {
+                    this.lockpicking = false;
+                }
+
+                bool lockpicked = player.Lockpick();
+                if (lockpicked)
+                {
+                    this.OpenOrClose();
+                    this.isLocked = false;
+                    this.lockpicking = false;
+                }
+            }
+        }
+
+
+
+        private void OpenOrClose()
+        {
+            bool isOpen = animator.GetBool("isOpenDoor");
+            animator.SetBool("isOpenDoor", !isOpen);
+        }
+
+
+    }
+
 }
