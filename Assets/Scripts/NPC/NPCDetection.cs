d04bcbba30f9269bc641ec400f290a3f1745fda4
DanielCorreia21 <danielcorreia210100@gmail.com>
2020-12-11 17:02:24 +0000

    Adicionado portas a fechar automaticamente depois de um delay

--- Assets/Scripts/NPC/NPCDetection.cs
+++ Assets/Scripts/NPC/NPCDetection.cs
@@ -8,6 +8,7 @@ public class NPCDetection : MonoBehaviour
     //Estes numeros podem ser melhorados sabendo melhor o tamanho exato do cone, que se pode ir buscar
     public float darkDistance = 9.0f;
     public float lightDistance = 14.5f;
+    public float doorCloseDelay = 3f;
     //Se calhar temos que ter diferentes para portas diferentes, e por Tags diferentes para cada tipo de porta
     //Mas pronto isto é só para portas pequenas nao abriem prematuramente, não é o fim do mundo
     public float doorRadius = 6f;
@@ -42,8 +43,26 @@ public class NPCDetection : MonoBehaviour
             Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
             if (hit.collider.gameObject.CompareTag("Door") && hit.distance < doorRadius)
             {
-                hit.collider.gameObject.GetComponent<Animator>().SetBool("isOpenDoor", true);
+                //hit.collider.gameObject.GetComponent<Animator>().SetBool("isOpenDoor", true);
+                Animator doorAnim = hit.collider.gameObject.GetComponent<Animator>();
+                bool isOpen = doorAnim.GetBool("isOpenDoor");
+                if (!isOpen)
+                {
+                    doorAnim.SetBool("isOpenDoor", true);
+                    StartCoroutine(CloseDoor(doorAnim, doorCloseDelay));
+                }
             }
+        }
+    }
+
+    private IEnumerator CloseDoor(Animator doorAnim, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        bool isOpen = doorAnim.GetBool("isOpenDoor");
+        if (isOpen)
+        {
+            doorAnim.SetBool("isOpenDoor", false);
+
         }
     }
 
