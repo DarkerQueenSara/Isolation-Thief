48c1724d9968eb241ed28bcd714be73d9c123ee9
Bernardo Pinto <bernardo.ra.pinto@gmail.com>
2020-12-16 17:43:38 +0000

    Clean old NPC stuff

---
+++ Assets/GoToPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GoToPoint : MonoBehaviour
+{
+    public Transform point;
+    NavMeshAgent m_Agent;
+    RaycastHit m_HitInfo = new RaycastHit();
+
+    void Start()
+    {
+        
+        m_Agent = GetComponent<NavMeshAgent>();
+        m_Agent.destination = point.position;
+    }
+
+}
--- Assets/Scripts/NPC/NPCMovement.cs
+++ Assets/Scripts/NPC/NPCMovement.cs
@@ -1,5 +1,4 @@
-﻿using Pathfinding;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -22,28 +21,11 @@ public class NPCMovement : MonoBehaviour
 
     public bool callingCops = false;
 
-    //Ir direto a uma direçao
-    private AIPath pathfinder;
-
-    //SETTERS DE DESTINO (só um ligado)    
-    //Ir em volta de um array de targets
-    private Patrol patroler;
-    //Escolher ao calhas à volta de um raio
-    private WanderingDestinationSetter wanderer;
-    //Ir direto
-    private AIDestinationSetter setter;
-
-    [SerializeField]
-    public List<PatrolTargets> roomsToPatrol;
     public List<Transform> telephones;
 
     // Start is called before the first frame update
     void Start()
     {
-        pathfinder = this.GetComponent<AIPath>();
-        patroler = this.GetComponent<Patrol>();
-        wanderer = this.GetComponent<WanderingDestinationSetter>();
-        setter = this.GetComponent<AIDestinationSetter>();
         patrolTimeLeft = patrolTime;
         wanderTimeLeft = wanderTime;
     }
@@ -57,49 +39,34 @@ public class NPCMovement : MonoBehaviour
             lastKnownDistractionPosition = null;
         }
 
-        //Se for preciso tenho de tirar o tempo dos timers
-        if (patroler.isActiveAndEnabled)
-        {
-            patrolTimeLeft -= Time.deltaTime;
-        }
-        if (wanderer.isActiveAndEnabled)
-        {
-            wanderTimeLeft -= Time.deltaTime;
-        }
-
         //Chamou os policias vai barricar-se no quarto
         if (GameManager.Instance.copsCalled)
         {
             //Debug.Log("Policias chamados");
-            GoTo(spawnPoint);
             patrolTimeLeft = patrolTime;
             wanderTimeLeft = wanderTime;
-            pathfinder.maxSpeed = 5.0f;
 
         }
         //Ele sabe onde está o player e vai chamar a policia
         else if (callingCops)
         {
             //Debug.Log("Vai chamar policias");
-            GoTo(GetClosestPhone());
+            
             patrolTimeLeft = patrolTime;
             wanderTimeLeft = wanderTime;
-            pathfinder.maxSpeed = 5.0f;
         }
         //Ele não sabe onde está o player, mas sabe do último barulho
         else if (lastKnownDistractionPosition != null)
         {
             //Debug.Log("Sabe de um som (de alguma forma)");
-            GoTo(lastKnownDistractionPosition);
             patrolTimeLeft = patrolTime;
             wanderTimeLeft = wanderTime;
-            pathfinder.maxSpeed = 2.0f;
         }
         //Ele não sabe onde está o player, já chegou ao barulho vai patrulhar a área
         else if (patrolTimeLeft > 0)
         {
             //Debug.Log("Vai patrulhar (de alguma forma)");
-            Patrol();
+
         }
         //Desistiu vai andar pela casa um bocado
         else //if (wanderTimeLeft > 0)
@@ -115,42 +82,6 @@ public class NPCMovement : MonoBehaviour
         */
     }
 
-    void GoTo(Transform target)
-    {
-        patroler.enabled = false;
-        wanderer.enabled = false;
-        setter.enabled = true;
-        setter.target = target;
-    }
-
-    void Patrol()
-    {
-        wanderer.enabled = false;
-        setter.enabled = false;
-        patroler.enabled = true;
-        patroler.targets = GetClosestRoom().targets;
-        pathfinder.maxSpeed = 2.0f;
-    }
-
-    PatrolTargets GetClosestRoom()
-    {
-        Vector3 NPC = this.gameObject.transform.position;
-        PatrolTargets aux = null;
-
-        foreach (PatrolTargets item in roomsToPatrol)
-        {
-            if (aux == null)
-            {
-                aux = item;
-            }
-            else if (Vector3.Distance(NPC, item.center.position) < Vector3.Distance(NPC, aux.center.position))
-            {
-                aux = item;
-            }
-        }
-
-        return aux;
-    }
 
     Transform GetClosestPhone()
     {
@@ -175,9 +106,5 @@ public class NPCMovement : MonoBehaviour
 
     void Wander()
     {
-        patroler.enabled = false;
-        setter.enabled = false;
-        wanderer.enabled = true;
-        pathfinder.maxSpeed = 2.0f;
     }
 }
