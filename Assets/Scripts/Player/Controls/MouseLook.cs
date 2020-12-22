c48badbd8f0964173695f1a9db05bf3cac1c7beb
Bernardo Pinto <bernardo.ra.pinto@gmail.com>
2020-12-22 22:28:06 +0000

    Fix player camera

--- Assets/Scripts/Player/Controls/MouseLook.cs
+++ Assets/Scripts/Player/Controls/MouseLook.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-	public float mouseSensitivity = 250f;
+	public float mouseSensitivity = 3f;
 
 	public Transform playerBody;
 
@@ -26,8 +26,8 @@ public class MouseLook : MonoBehaviour
 			return;
 		}
 
-		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+		float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+		float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
 
 		//rotation is flipped
 		xRotation -= mouseY;
