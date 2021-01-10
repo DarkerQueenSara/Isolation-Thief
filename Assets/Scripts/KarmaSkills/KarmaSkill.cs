0ca497d473f74467f89294ffd9ecd87f3303ee3d
DanielCorreia21 <danielcorreia210100@gmail.com>
2021-01-10 23:25:46 +0000

    Skeleton for KarmaSkills

---
+++ Assets/Scripts/KarmaSkills/KarmaSkill.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class KarmaSkill
+{
+
+    public bool unlocked;
+    public const string SKILL_INFO_DIR = "SkillInfos/";
+    public SkillInfo skillInfo;
+    public KarmaSkill parent;
+    public List<KarmaSkill> skillDependencies;
+
+    public KarmaSkill()
+    {
+        skillDependencies = new List<KarmaSkill>();
+        unlocked = false;
+        parent = null;
+    }
+
+    public void setParent(KarmaSkill parent)
+    {
+        this.parent = parent;
+    }
+
+    public bool canUnlock()
+    {
+        return GameManager.Instance.availableXp > this.getXPCost();
+    }
+    
+    public void unlock()
+    {
+        if (canUnlock())
+        {
+            GameManager.Instance.availableXp -= this.getXPCost();
+            this.unlocked = true;
+        }
+    }
+
+    public abstract void activate();
+    public abstract string getID();
+
+    public string getName()
+    {
+        return this.skillInfo.name;
+    }
+
+    public int getXPCost()
+    {
+        return this.skillInfo.xpCost;
+    }
+
+    public Sprite getSprite()
+    {
+        return this.skillInfo.GetSprite();
+    }
+}
---
+++ Assets/Scripts/KarmaSkills/KarmaSkillsTree.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class KarmaSkillsTree
+{
+    public Dictionary<string, KarmaSkill> skills;
+
+    public KarmaSkillsTree()
+    {
+        this.skills = new Dictionary<string, KarmaSkill>();
+    }
+
+    public KarmaSkill GetSkill(string skillName)
+    {
+        KarmaSkill skill = skills.ContainsKey(skillName) ? skills[skillName] : null;
+
+        if (skill != null)
+        {
+            return skill;
+        }
+
+        else return null;
+
+    }
+
+    public List<KarmaSkill> getUnlockedSkills()
+    {
+        List<KarmaSkill> unlockedSkills = new List<KarmaSkill>();
+        foreach (KarmaSkill skill in this.skills.Values)
+        {
+            if (skill.unlocked)
+            {
+                unlockedSkills.Add(skill);
+            }
+        }
+        return unlockedSkills;
+    }
+
+    public List<KarmaSkill> getAllSkills()
+    {
+        return new List<KarmaSkill>(this.skills.Values);
+    }
+
+    public void activateAllSkills()
+    {
+        Debug.Log("Activating skills");
+        foreach(KarmaSkill skill in this.skills.Values)
+        {
+            skill.activate();
+        }
+    }
+
+}
