59f8a9312e9b907c31a2c56260ad9ff77f2731d5
lidiagc <lidiagcustodio@gmail.com>
2021-01-11 10:21:19 +0000

    skills element menu with functions

---
+++ Assets/Scripts/Skills/SkillElementUI.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+public class SkillElementUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
+{
+	public Animator animator;
+	private SkillsTree skillsTree;
+	public UnityEvent addFunction;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		animator = transform.Find("Button").GetComponent<Animator>();
+		this.skillsTree = GameManager.Instance.skillsTree;
+	}
+
+	public void OnPointerEnter(PointerEventData e)
+	{
+		animator.SetBool("selected", true);
+	}
+
+	public void OnPointerExit(PointerEventData e)
+	{
+		animator.SetBool("selected", false);
+		animator.SetBool("pressed", false);
+	}
+
+	public void OnPointerClick(PointerEventData e)
+	{
+		animator.SetBool("pressed", true);
+		addFunction.Invoke();
+	}
+
+	public void TestAction()
+	{
+		Debug.Log("Skill button test action");
+	}
+
+	void AddSkill(string skillID, string skillName)
+	{
+		Skill skill = skillsTree.GetSkill(skillID);
+		if (skill.canUnlock())
+		{
+			Debug.Log("unlocking skill!");
+			skill.unlock();
+			Image button = transform.Find("Button").GetComponent<Image>();
+			TextMeshProUGUI price = transform.Find("Price").GetComponent<TextMeshProUGUI>();
+			SkillsTreeUI.Instance.updateVisualUnocked(button, price);
+			SkillsTreeUI.Instance.updateHelpText(skillName + " unlocked");
+		}
+		else
+		{
+			Debug.Log("Can't unlock skill!");
+			SkillsTreeUI.Instance.updateHelpText("Can't unlock " + skillName);
+		}
+	}
+
+	public void AddJumpman()
+	{
+		AddSkill(Jumpman.ID, "Jumpman");
+	}
+
+	public void AddJumpman2()
+	{
+		AddSkill(Jumpman2.ID, "Jumpman 2");
+	}
+
+	public void AddLightStep()
+	{
+		AddSkill(LightStep.ID, "Light Step");
+	}
+
+	public void AddLightStep2()
+	{
+		AddSkill(LightStep2.ID, "Light Step 2");
+	}
+
+}
---
+++ Assets/Scripts/Skills/SkillsTreeUI.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SkillsTreeUI : MonoBehaviour
+{
+	public static SkillsTreeUI Instance { get; private set; }
+
+	public SkillsTree skillsTree;
+
+	private void Awake()
+	{
+		Instance = this;
+	}
+
+	void disableButton(Button button)
+	{
+		//button.enabled = false;
+		button.transform.gameObject.SetActive(false);
+	}
+
+	public void updateVisualUnocked(Image button, TextMeshProUGUI priceText)
+	{
+		button.transform.gameObject.SetActive(false);
+		priceText.transform.gameObject.SetActive(false);
+		updateExperienceText();
+	}
+
+	public void updateHelpText(string text)
+	{
+		TextMeshProUGUI HelpText = transform.Find("HelpText").GetComponent<TextMeshProUGUI>();
+		HelpText.text = text;
+		HelpText.GetComponent<Animator>().Play("FadeOut");
+		HelpText.transform.gameObject.SetActive(true);
+	}
+
+	void updateSkillPriceText(string skillUILabel, string skillID)
+	{
+		TextMeshProUGUI skillPrice = transform.Find("Tree").Find(skillUILabel).Find("Price").GetComponent<TextMeshProUGUI>();
+		skillPrice.text = skillsTree.GetSkill(skillID).getXPCost().ToString() + " XP";
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		this.skillsTree = GameManager.Instance.skillsTree;
+
+		updateExperienceText();
+
+		// Update gadget price text
+		updateSkillPriceText("Jumpman", Jumpman.ID);
+		updateSkillPriceText("Jumpman2", Jumpman2.ID);
+		updateSkillPriceText("LightStep", LightStep.ID);
+		updateSkillPriceText("LightStep2", LightStep2.ID);
+
+	}
+
+	private void updateExperienceText()
+	{
+		transform.Find("Experience").GetComponent<TextMeshProUGUI>().text = GameManager.Instance.availableXp.ToString() + " XP";
+	}
+
+}
