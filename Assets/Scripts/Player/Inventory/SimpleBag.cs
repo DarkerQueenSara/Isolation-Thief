81c8ea51980f1ffcfbd704f52f101ced03f0bae2
DanielCorreia21 <danielcorreia210100@gmail.com>
2021-01-12 17:39:04 +0000

    didnt save before commiting

--- Assets/Scripts/Player/Inventory/SimpleBag.cs
+++ Assets/Scripts/Player/Inventory/SimpleBag.cs
@@ -4,16 +4,9 @@ using UnityEngine;
 
 public class SimpleBag : Inventory
 {
-
-
-    // Start is called before the first frame update
-
     public SimpleBag()
     {
-        //this.Items.Add(new Item { itemType = Item.ItemType.TV, amount = 1});
-        //this.Items.Add(new Item { itemType = Item.ItemType.Tablet, amount = 1 });
-        //this.Items.Add(new Item { itemType = Item.ItemType.Cash, amount = 5 });
-        //this.Items.Add(new Item { itemType = Item.ItemType.Phone, amount = 2 });
+        
     }
 
     public override void AddItem(Item item, GameObject itemObject)
