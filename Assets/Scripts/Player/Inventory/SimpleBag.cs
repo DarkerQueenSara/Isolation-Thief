7b37b7abad8b8ce57565ee54adf57de16735e9a5
lidiagc <lidiagcustodio@gmail.com>
2021-01-13 14:43:15 +0000

    help text when inventory is full and moved truck closer to the ground to be easir to get in

--- Assets/Scripts/Player/Inventory/SimpleBag.cs
+++ Assets/Scripts/Player/Inventory/SimpleBag.cs
@@ -4,25 +4,26 @@ using UnityEngine;
 
 public class SimpleBag : Inventory
 {
-    public SimpleBag()
-    {
-        
-    }
+	public SimpleBag()
+	{
 
-    public override void AddItem(Item item, GameObject itemObject)
-    {
-        if(item.weight + TotalWeight <= MAX_WEIGHT + 0.00001)
-        {
-            counter++;
-            this.TotalValue += item.value;
-            this.TotalWeight += item.weight;
-            itemObject.SetActive(false);
-            ItemWrapper itemWrapper = new ItemWrapper(item, counter, itemObject);
-            this.Items.Add(itemWrapper);
-        }
-        else
-        {
-            //Launch error saing that inventory is too heavy to add item
-        }
-    }
+	}
+
+	public override void AddItem(Item item, GameObject itemObject)
+	{
+		if (item.weight + TotalWeight <= MAX_WEIGHT + 0.00001)
+		{
+			counter++;
+			this.TotalValue += item.value;
+			this.TotalWeight += item.weight;
+			itemObject.SetActive(false);
+			ItemWrapper itemWrapper = new ItemWrapper(item, counter, itemObject);
+			this.Items.Add(itemWrapper);
+		}
+		else
+		{
+			HelpText.instance.updateHelpText("Inventory is too heavy to add this item");
+			//Launch error saing that inventory is too heavy to add item
+		}
+	}
 }
---
+++ Assets/Scripts/UI/HelpText.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class HelpText : MonoBehaviour
+{
+	TextMeshProUGUI textUI;
+	bool hasText;
+
+
+	#region SINGLETON
+	public static HelpText instance;
+
+	private void Awake()
+	{
+		if (instance != null)
+		{
+			Debug.LogWarning("More than one instance of HelpText found!");
+		}
+		instance = this;
+		textUI = GetComponent<TextMeshProUGUI>();
+	}
+	#endregion
+
+	public void updateHelpText(string text)
+	{
+		this.textUI.text = text;
+		textUI.text = text;
+		textUI.GetComponent<Animator>().Play("FadeOut");
+		textUI.transform.gameObject.SetActive(true);
+	}
+
+	public bool HasText()
+	{
+		return hasText;
+	}
+}
